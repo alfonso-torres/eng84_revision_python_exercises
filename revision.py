f13c0a7f97cfb9eb911d255cb11b79986cd892f5
alfonso-torres <alfonso_sepul@hotmail.com>
2021-03-31 10:54:58 +0100

    exercise of lists and tuples done.

--- revision.py
+++ revision.py
@@ -19,3 +19,23 @@
 
 if "a" == "b" and "c" == "d":
     print(True)
+
+# Create a list of five numbers, starting from zero
+
+numbers = [0, 1, 2, 3, 4]
+
+#for num in range(5):
+#    numbers.append(num)
+
+# Create a tuple with the same information
+
+tuple_number = (0, 1, 2, 3, 4)
+#numbers_tuple = tuple(numbers)
+#print(tuple_number)
+#print(numbers_tuple)
+
+# Change the value fot the tuple in the last index
+
+# CAN NOT CHANGE A TUPLE
+# BECAUSE TUPLES ARE IMMUTABLE
+# UNLIKE LISTS, THAT CAN BE CHANGED AS THEY ARE MUTABLE
