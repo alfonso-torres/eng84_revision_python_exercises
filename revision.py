b5e2836aba1d926a794f2d3db510efc47d0adb2f
alfonso-torres <alfonso_sepul@hotmail.com>
2021-03-31 10:45:48 +0100

    exercise of operators done.

--- revision.py
+++ revision.py
@@ -1,5 +1,21 @@
 # Create a function called greetings that takes one argument as string and returns the name
 
-def greeting(name):
-    return "Welcome " + name
+#def greeting(name):
+#    return "Welcome " + name
 
+#print(greeting("Jose"))
+
+# Declare a list of numbers from 1 - 9.
+# Iterate through the list and print the list
+# declare, list, iterate, print
+#numList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+#for item in numList:
+#    print(item)
+
+# Write boolean operators
+# If a == b and c == d print true
+# write an example of the boolean operators
+
+if "a" == "b" and "c" == "d":
+    print(True)
